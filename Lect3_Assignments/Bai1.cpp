da5bc498715def0196b8b738f4cab10aa5955c6d
23021428 <23021428@vnu.edu.vn>
2025-03-29 11:18:42 +0700

    Update Bai1.cpp

--- Lect3_Assignments/Bai1.cpp
+++ Lect3_Assignments/Bai1.cpp
@@ -1,11 +1,22 @@
-1. 2^10 → O(1) 
-2. 4n → O(n) 
-3. 3n + 100logn → O(n) 
-4. 2^logn → O(n) 
-5. nlogn → O(n log n)
-6. 4n log n + 2n → O(n log n)
-7. n^2 + 10n → O(n^2)
-8. n^2 → O(n^2)
-9. n^3 → O(n^3)
-10. 2n → O(2^n) 
-SORT: 2^10 < 4n < 3n + 100logn < 2^logn < nlogn < 4n log n + 2n < n^2 + 10n < n^2 < n^3 < 2n
+/*
+    Biến đổi:
+        4nlogn + 2n -> O(nlogn)
+        2^10        -> O(1)
+        3n+100logn  -> O(n)
+        2^logn      -> O(n)
+        4n          -> O(n)
+        n^2 + 10n   -> O(n^2)
+        2^n         -> O(2^n)
+        n^3         -> O(n^3)
+        nlogn       -> O(nlogn)
+    Sắp xếp:
+        2^10
+        2^logn
+        4n
+        3n + 100logn
+        nlogn
+        4nlogn + 2n
+        n^2 + 10n
+        n^3
+        2^n
+*/
