d9db5ca663b7821877be689da45fd04805570a37
23021428 <23021428@vnu.edu.vn>
2025-04-18 23:09:22 +0700

    Create Bai2.cpp

---
+++ Lect7_Assignments/Bai2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) { // Hàm tính giai thừa của n
+	if (n == 0 || n == 1) return 1;
+	return n * factorial(n - 1); // Hàm trả về n nhân với đệ quy của nó
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int x = factorial(n);
+	cout << x << endl;
+	return 0;
+}
